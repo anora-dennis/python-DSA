07f189414801f110fb68e0edccfb4a314e3877ac
Anora Dennis <anoradennis098@gmail.com>
2025-08-15 18:27:21 +0000

    commit 1

---
+++ indexing.py
@@ -0,0 +1,20 @@
+# Sequences: string, list, tuple
+
+# string 
+x = 'frog'
+
+# will print 'g' since python counts from 0
+print(x[3])
+
+# list
+y = ['pig', 'cow', 'horse']
+
+#will print 'cow'
+print(y[1])
+
+# tuples cannot be modified later on
+z = ('kevin', 'Niklas', 16)
+
+# will print 16
+print(z[2])
+
---
+++ iterating.py
@@ -0,0 +1,14 @@
+# printing every item in x
+x = [7, 8, 9]
+for item in x:
+    print(item)
+
+# printing both the index and the item
+y = [1, 2, 3]
+for index, item in enumerate(y):
+    print(index, item)
+
+# length of items: len
+# slicing ':'
+# min and max are alphebatic 
+# sorting: alphebatic 
